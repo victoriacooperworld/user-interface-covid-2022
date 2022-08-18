5c9a57b33f8f4a1923d4ec17d57d68a8780ff7f5
C Glabe <victoriacoo98@yahoo.com>
2022-08-18 15:42:15 -0700

    Added components

---
+++ client/user/src/components/operationArea/operation.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Button from '@mui/material/Button';
+const OperationArea = () =>{
+    return(
+        <div>
+            <Button>Upload Patient Data</Button>
+            <Button>Search Protein</Button>
+            {/* <h3>OperationArea</h3> */}
+        </div>
+    )
+}
+
+export default OperationArea;
