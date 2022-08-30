55a4d247150bc8fd85aabe14124d6abf785ef94f
KeaneWong <Keanewong913@gmail.com>
2022-08-30 13:30:15 -0700

    Updated flask server

--- client/user/src/App.js
+++ client/user/src/App.js
@@ -1,15 +1,21 @@
 import './App.css';
 import Author from './components/author/author';
 import OperationArea from './components/operationArea/operation';
+import TetramerGet from './components/TetramerGet';
+import {BrowserRouter, Routes, Switch, Route} from 'react-router-dom'
+
 function App() {
   return (
+
     <div className="App">
+
       <h1>Hello World</h1>
       <Author/>
       <OperationArea/>
-    
-
+      <TetramerGet/>
     </div>
+
+
   );
 }
 
