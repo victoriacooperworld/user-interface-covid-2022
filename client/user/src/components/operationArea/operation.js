e9c9fdb8726865de9db6070f175f011d29a6e314
KeaneWong <Keanewong913@gmail.com>
2022-09-13 12:41:14 -0700

    Now accepting two types of uploaded files. Whenever it uploads it removes all files from directory it downloads into

--- client/user/src/components/operationArea/operation.js
+++ client/user/src/components/operationArea/operation.js
@@ -2,60 +2,61 @@ import {React, useState} from "react";
 import './operation.css';
 
 const OperationArea = () =>{
-    const [file, setFile] = useState();
-    const [filePicked, setFilePicked] = useState(false);
+    const [file1,setFile1] = useState();
+    const [file2,setFile2] = useState();
+    const [filePicked1, setFilePicked1] = useState(false);
+    const [filePicked2, setFilePicked2] = useState(false);
+    const [patientDatasPos, setpatientDatasPos] = useState([])
+    const [patientDatasNeg, setpatientDatasNeg] = useState([])
+
     const [searchProt, setSearchProt] = useState("");
 
-    /* 
-    const sendFilePath = event =>{
-        event.preventDefault();
-        console.log(filePath)
-        if(filePath === ""){
-            console.log("ERROR: FILEPATH EMPTY")
-        }
-        else{
-            try{
-                fetch('http://localhost:5000/ProcessData', {
-                    method: "POST",
-                    headers: {
-                        'Content-type': 'application/json'
-                      },
-                      body: JSON.stringify(filePath)
-                }).then(
-                    res => 
-                        res.json()
-                    
-                    //do something with the responnse
-                ).then(
-                    jsonRet => console.log(jsonRet)
-                  //do sometjing with the response
-                )
-            }
-            catch(err){
-                console.log(err)
-            }
-            
-        }
-    }
-    */
-
     /* Displays uploaded file information to user */
-    const fileChangeHandler = (event) => {
-        setFile(event.target.files[0]);
-        setFilePicked(true);
-        let data = new FormData()
+    const fileChangeHandler1 = (event) => {
+        setFile1(event.target.files[0]);
+        setFilePicked1(true);
+        let newPatientData = []
         for (let i = 0; i < event.target.files.length; i++) 
         {
-            data.append("file", event.target.files[i]);
+            setpatientDatasPos(result => result.concat(event.target.files[i]))
         }
-        // console.log(event.target.files)
-        // console.log(data.getAll('file'))
+        console.log(patientDatasPos)
+       
+    };
+    const fileChangeHandler2 = (event) => {
+        setFile2(event.target.files[0]);
+        setFilePicked2(true);
+        let newPatientData = []
+        for (let i = 0; i < event.target.files.length; i++) 
+        {
+            setpatientDatasNeg(result => result.concat(event.target.files[i]))
+        }
+        console.log(patientDatasNeg)
+
+    };
+
+    /* Handles file submission to server */
+    const fileSubmitHandler = (event) => {
+        event.preventDefault();
+ 
+        if(!filePicked1 || !filePicked2){
+            console.log("ERROR: One of the files were not set")
+            return
+        }
+        let data = new FormData()
+        for (let i =0; i < patientDatasPos.length;i++)
+        {
+            data.append("file1",patientDatasPos[i])
+        }
+        for (let i =0; i < patientDatasNeg.length;i++)
+        {
+            data.append("file2",patientDatasNeg[i])
+        }
         try
         {
             fetch('http://localhost:5000/Uploads',{
                 method: "POST",
-                mode: 'no-cors',
-  
+                
                 body: data
 
             }).then((response) =>
@@ -68,11 +69,6 @@ const OperationArea = () =>{
         {
             console.log(err)
         }
-    };
-
-    /* Handles file submission to server */
-    const fileSubmitHandler = (event) => {
-        //TODO:
     };
 
     /* Hanldes submission of text entered in protein search */
@@ -103,20 +99,31 @@ const OperationArea = () =>{
     return(
         <div className="operations">
             <form className="fileupload" onSubmit = {fileSubmitHandler}>
-                <input className="upload" directory="" webkitdirectory="" type="file" onChange={fileChangeHandler}></input>
+                <input className="upload" directory="" webkitdirectory="" type="file" onChange={fileChangeHandler1}></input>
 
                 {/* Render conditionally if file is chosen or not chosen */}
-                {filePicked ? (
+                {filePicked1 ? (
                     <div className="fileinfo">
-                        <p>Name: {file.name}</p>
-                        <p>Type: {file.type}</p>
-                        <p>Size: {parseInt(file.size / 100000)}kb</p>
+                        <p>Name: {file1.name}</p>
+                        <p>Type: {file1.type}</p>
+                        <p>Size: {parseInt(file1.size / 100000)}kb</p>
                     </div>
                 ): (
                     <p></p>
                 )}
 
-                <button className="button" type="submit" >Upload Patient Data</button>
+                <input className="upload" directory="" webkitdirectory="" type="file" onChange={fileChangeHandler2}></input>
+                {/* Render conditionally if file is chosen or not chosen */}
+                {filePicked2 ? (
+                    <div className="fileinfo">
+                        <p>Name: {file2.name}</p>
+                        <p>Type: {file2.type}</p>
+                        <p>Size: {parseInt(file2.size / 100000)}kb</p>
+                    </div>
+                ): (
+                    <p></p>
+                )}
+                <button className="button" type="submit" >Process Patient Data</button>
             </form>
             
            <form className="proteinsearch" onSubmit = {SearchProtein}>
--- server/server.py
+++ server/server.py
@@ -1,9 +1,20 @@
 import os
+from aiohttp import ClientRequest
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
-
+import shutil, os
 app = Flask(__name__)
 
+def ClrDirectory(folder):
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if filename.endswith('.fna'):
+                os.remove(file_path)
+            else:
+                continue
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
 
 @app.route("/members")
 @cross_origin()
@@ -39,21 +50,38 @@ def SearchProtein(searchProt):
     
     return jsonify(searchProt + searchProt)
 
+
+
+def ProcessPatientData(uploads_dir1, uploads_dir2):
+    
+    pass
+
 @app.route("/Uploads", methods=['POST'])
 @cross_origin()
 def upload():
-    
-    uploads_dir = '/Users/keanewong/Desktop/User-interface-covid2022/Uploads'
-    uploaded_files = request.files.getlist('file')
-    # uploaded_files = request.files['file']
-    # print(uploaded_files)
-    if 'file' not in request.files:
+    print("Using Upload")
+    uploads_dir1 = '/Users/keanewong/Desktop/User-interface-covid2022/Uploads/Pos'
+    uploads_dir2 = '/Users/keanewong/Desktop/User-interface-covid2022/Uploads/Neg'
+    ClrDirectory(uploads_dir1)
+    ClrDirectory(uploads_dir2)
+    uploaded_files1 = request.files.getlist('file1')
+    uploaded_files2 = request.files.getlist('file2')
+    if 'file1' not in request.files:
+        print("No file1 sent")
+    if 'file2' not in request.files:
         print("No file sent")
-    print("Upload function called, uploading ", len(uploaded_files), " to server")
-    print("Saving in ", uploads_dir)
-    for file in uploaded_files:
-        file.save(os.path.join(uploads_dir, file.filename.split('/')[1]))
-    return jsonify("Uploads completed")
+    print("Upload function called, uploading ", len(uploaded_files1), " positive to server")
+    print("Upload function called, uploading ", len(uploaded_files2), " negative to server")
+    print("Saving in ", uploads_dir1)
+    print("Saving in ", uploads_dir2)
+    for file in uploaded_files1:
+        file.save(os.path.join(uploads_dir1, file.filename.split('/')[1]))
+    for file in uploaded_files2:
+        file.save(os.path.join(uploads_dir2, file.filename.split('/')[1]))
+    return jsonify({"Message":"Uploads completed"})
+
+
+
 
 if(__name__) == "__main__":
     app.run(debug=True)
