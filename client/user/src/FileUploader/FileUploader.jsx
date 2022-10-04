e9c872ad688042cdac71183845652de335eaca59
tlilley <tlilley@uci.edu>
2022-10-04 12:30:56 -0700

    updated UI

--- client/user/src/FileUploader/FileUploader.jsx
+++ client/user/src/FileUploader/FileUploader.jsx
@@ -1,32 +1,33 @@
-import React from 'react'
-
 /**
-* @author
-* @function FileUploader
-**/
-import './style.css'
+ * @author
+ * @function FileUploader
+ **/
+import React from 'react';
+import { HiArrowSmUp } from "react-icons/hi";
+import './FileUploader.css'
 
 
-export const FileUploader = (props) => {
+export const FileUploader = ({buttonText, callBack}) => {
     const hiddenFileInput = React.useRef(null);
     const handleClick = event => {
         hiddenFileInput.current.click();
       };
   return(
-  <>
-    <button onClick={handleClick} className = "FileUploader">
-      {props.buttonText}
+  <div className='button-container'>
+    <button onClick={handleClick} className='upload-button'>
+      <HiArrowSmUp/>
+      {buttonText}
     </button>
     <input
       type="file"
       ref={hiddenFileInput}
-      onChange={props.callBack}
+      onChange={callBack}
       style={{display: 'none'}} 
       accept = '.fna, .txt'
       webkitdirectory = "" 
       directory = ""
     />
-  </>
+  </div>
    )
 
  }
